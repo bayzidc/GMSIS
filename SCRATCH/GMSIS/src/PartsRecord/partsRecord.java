4fb658cc0b57d2adbbab69a000b6e5000032cbb3
Hamza Busuri <h.busuri@se15.qmul.ac.uk>
2017-01-18 22:25:01 +0000

    Project

---
+++ SCRATCH/GMSIS/src/PartsRecord/partsRecord.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PartsRecord;
+
+/**
+ *
+ * @author User
+ */
+public class partsRecord {
+    public static void main(String[] args)
+    {
+        System.out.println("Fabiha");
+    }
+    
+}
