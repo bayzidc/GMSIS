12633be00428b82652640840ff509c16508db43a
Fabiha <Fabiha@100.66.8.89>
2017-02-09 11:40:47 +0000

    Added parts record fxml.

---
+++ SCRATCH/GMSIS/src/PartsRecord/gui/PartsController.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PartsRecord.gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Fabiha
+ */
+public class PartsController implements Initializable {
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField description;
+    @FXML
+    private TextField idNumber;
+    @FXML
+    private Button backButton;
+    @FXML
+    private Button trackPartsWithdraw;
+    @FXML
+    private Button trackPartsAddition;
+    
+    
+    @FXML
+    private void backButton(ActionEvent event) throws IOException
+    {
+        Parent adminUser = FXMLLoader.load(getClass().getResource("/Authentication/Admin.fxml"));
+        Scene admin_Scene = new Scene(adminUser);
+        Stage stage2 = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage2.hide();           
+        stage2.setScene(admin_Scene);
+        stage2.show();
+        
+    }
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
