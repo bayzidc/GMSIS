fdd0eba24f293a1908cb6fd72c65da62e0279e9f
Joen <Joen@DESKTOP-QG27V57>
2017-02-13 23:26:18 +0000

    Many things updated: Able to add users, with error checking to make sure all fields are correct and not emtpy. Able to update users, with error checking to make sure it's successful and all fields are complete and not correct. Able to delete users, with error checking making sure a user is selected before delecting, and making sure user wants to delete the customer.

--- SCRATCH/GMSIS/src/CustomerAccount/gui/GuiController.java
+++ SCRATCH/GMSIS/src/CustomerAccount/gui/GuiController.java
@@ -27,7 +27,10 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import CustomerAccount.logic.customerAccount;
-
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -56,6 +59,8 @@ public class GuiController implements Initializable {
     private TextField phoneText;
     @FXML
     private TextField emailText;
+    @FXML
+    private TextField accTypeText;
     @FXML
     private TableView<customerAccount> table;
     @FXML
@@ -86,6 +91,138 @@ public class GuiController implements Initializable {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    private void addButton(ActionEvent event) throws IOException, ClassNotFoundException {
+        createData();
+        buildData();
+    }
+
+    @FXML
+    private void deleteButton(ActionEvent event) throws IOException, ClassNotFoundException {
+        try {
+            String confirmDelete = JOptionPane.showInputDialog("Are you sure you want to delete this user? (Yes or No) ");
+            if (confirmDelete.equalsIgnoreCase("Yes") && isDeleted()) {
+                int ID = table.getSelectionModel().getSelectedItem().getCustomerID(); //Gets ID 
+                JOptionPane.showMessageDialog(null, "UserID: " + ID + " has been deleted");
+                buildData();
+            }
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION); // Pop up box
+            alert.setTitle("Information");
+            alert.setHeaderText(null);
+            alert.setContentText("No user selected. Try again.");
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    private void updateButton(ActionEvent event) throws IOException, ClassNotFoundException {
+        try {
+            int check = Integer.parseInt(phoneText.getText());
+            if (fullNameText.getText().equals("") || addressText.getText().equals("") || postCodeText.getText().equals("") || check < 1 || emailText.getText().equals("") || accTypeText.getText().equals("")) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION); // Pop up box
+                alert.setTitle("Information");
+                alert.setHeaderText(null);
+                alert.setContentText("One or more fields are incomplete or incorrect.");
+                alert.showAndWait();
+            }
+            String confirmDelete = JOptionPane.showInputDialog("Are you sure you want to update this user? (Yes or No) ");
+            if (confirmDelete.equalsIgnoreCase("Yes")) {
+                updateData();
+                int ID = table.getSelectionModel().getSelectedItem().getCustomerID(); //Gets ID 
+                JOptionPane.showMessageDialog(null, "UserID: " + ID + " has been updated");
+                buildData();
+            }
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION); // Pop up box
+            alert.setTitle("Information");
+            alert.setHeaderText(null);
+            alert.setContentText("Please make sure all fields are completed and correct before proceding.");
+            alert.showAndWait();
+        }
+    }
+
+    private boolean isDeleted() throws ClassNotFoundException {
+        boolean customerDeleted = false;
+
+        int ID = table.getSelectionModel().getSelectedItem().getCustomerID();
+
+        Connection conn = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:database.sqlite");
+
+            System.out.println("Opened Database Successfully");
+            String sql = "DELETE FROM customer WHERE customer_id= ?";
+            PreparedStatement state = conn.prepareStatement(sql);
+            state.setInt(1, ID);
+            state.executeUpdate();
+
+            state.close();
+            conn.close();
+
+            customerDeleted = true;
+
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR); // Pop up box
+            alert.setTitle("Information");
+            alert.setHeaderText(null);
+            alert.setContentText("Something went wrong.");
+            alert.showAndWait();
+        }
+        return customerDeleted;
+
+    }
+
+    @FXML
+    private void updateData() throws ClassNotFoundException {
+
+        //System.out.println("SELECT * FROM NewUsers WHERE FirstName= " + "'" + firstName.getText() + "'" + "AND Surname= " + "'" + surname.getText() + "'" + "AND UserID= " + "'" + newUserID.getText() + "'");
+        Connection conn = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:database.sqlite");
+
+            int ID = table.getSelectionModel().getSelectedItem().getCustomerID();
+
+            System.out.println("Opened Database Successfully");
+
+            String sql = "UPDATE customer SET customer_fullname=?,customer_address=?,customer_postcode=?,customer_phone=?,customer_email=?,customer_type=? WHERE customer_id=?";
+            PreparedStatement state = conn.prepareStatement(sql);
+            int check = Integer.parseInt(phoneText.getText());
+            if (fullNameText.getText().equals("") || addressText.getText().equals("") || postCodeText.getText().equals("") || check < 1 || emailText.getText().equals("") || accTypeText.getText().equals("")) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION); // Pop up box
+                alert.setTitle("Information");
+                alert.setHeaderText(null);
+                alert.setContentText("One or more fields are incomplete or incorrect.");
+                alert.showAndWait();
+            } else {
+                state.setString(1, fullNameText.getText());
+                state.setString(2, addressText.getText());
+                state.setString(3, postCodeText.getText());
+                state.setInt(4, Integer.parseInt(phoneText.getText()));
+                state.setString(5, emailText.getText());
+                state.setString(6, accTypeText.getText());
+                state.setInt(7, ID);
+
+                state.execute();
+
+                state.close();
+                conn.close();
+
+            }//submit=true;
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR); // Pop up box
+            alert.setTitle("Information");
+            alert.setHeaderText(null);
+            alert.setContentText("Something went wrong.");
+            alert.showAndWait();
+        }
+
     }
 
     public void buildData() {
@@ -124,6 +261,50 @@ public class GuiController implements Initializable {
                 new PropertyValueFactory<>("customerEmail"));
         customerType.setCellValueFactory(
                 new PropertyValueFactory<>("customerType"));
+    }
+
+    public void createData() throws ClassNotFoundException {
+
+        Connection conn = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:database.sqlite");
+
+            System.out.println("Creating data.");
+
+            String sql = "insert into customer(customer_fullname, customer_address, customer_postcode, customer_phone, customer_email, customer_type) values(?,?,?,?,?,?)";
+            PreparedStatement state = conn.prepareStatement(sql);
+            int check = Integer.parseInt(phoneText.getText());
+            if (fullNameText.getText().equals("") || addressText.getText().equals("") || postCodeText.getText().equals("") || check < 1 || emailText.getText().equals("") || accTypeText.getText().equals("")) {
+                Alert alert = new Alert(Alert.AlertType.INFORMATION); // Pop up box
+                alert.setTitle("Information");
+                alert.setHeaderText(null);
+                alert.setContentText("One or more fields are incomplete or incorrect.");
+                alert.showAndWait();
+            } else {
+                state.setString(1, fullNameText.getText());
+                state.setString(2, addressText.getText());
+                state.setString(3, postCodeText.getText());
+                state.setInt(4, Integer.parseInt(phoneText.getText()));
+                state.setString(5, emailText.getText());
+                state.setString(6, accTypeText.getText());
+
+                state.execute();
+
+                state.close();
+                conn.close();
+
+            }//submit=true;
+        } catch (Exception e) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION); // Pop up box
+            alert.setTitle("Information");
+            alert.setHeaderText(null);
+            alert.setContentText("One or more fields are incomplete or incorrect.");
+            alert.showAndWait();
+        }
+        //return submit;       
+
     }
 
 }
