3fed60854b5a5267c80d27b6c0f33b148353c830
Joen <Joen@DESKTOP-QG27V57>
2017-03-14 13:16:19 +0000

    Fixed login window.

--- SCRATCH/GMSIS/src/Authentication/Login.java
+++ SCRATCH/GMSIS/src/Authentication/Login.java
@@ -23,6 +23,7 @@ public class Login extends Application {
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.setResizable(true);
+            stage.setMaximized(true);
             stage.show();
 
         } catch (Exception e) {
